ddb2373222f85c154227919d66d6b0a0333a9e11
aqim <folly77folly@gmail.com>
2020-08-10 04:07:39 +0100

    removed webbrower

--- foodvendor/views/views.py
+++ foodvendor/views/views.py
@@ -6,4 +6,3 @@ import webbrowser
 
 def index(request):
     return HttpResponse("<h3><a href='https://documenter.getpostman.com/view/8806253/SztBa7TL?version=latest#64f8e2f7-353e-4931-9594-442970a1b59f][docs'>Welcome to My API Documentation for this project</a></h3>")
-    # webbrowser.open("https://documenter.getpostman.com/view/8806253/SztBa7TL?version=latest#64f8e2f7-353e-4931-9594-442970a1b59f][docs")
