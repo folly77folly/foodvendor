7b4b04be7404bc25b71c3e3a80027708c1630f22
aqim <folly77folly@gmail.com>
2020-08-10 04:04:42 +0100

    updated to a URL Link

--- foodvendor/views/views.py
+++ foodvendor/views/views.py
@@ -5,5 +5,5 @@ import webbrowser
 # Create your views here.
 
 def index(request):
-    # return HttpResponse("<h3>Welcome to VGG internship project<h3>")
-    webbrowser.open("https://documenter.getpostman.com/view/8806253/SztBa7TL?version=latest#64f8e2f7-353e-4931-9594-442970a1b59f][docs")
+    return HttpResponse("<h3><a href='https://documenter.getpostman.com/view/8806253/SztBa7TL?version=latest#64f8e2f7-353e-4931-9594-442970a1b59f][docs'>Welcome to My API Documentation for this project</a></h3>")
+    # webbrowser.open("https://documenter.getpostman.com/view/8806253/SztBa7TL?version=latest#64f8e2f7-353e-4931-9594-442970a1b59f][docs")
